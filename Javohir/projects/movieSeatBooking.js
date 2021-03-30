caeee1dd5ae806ab77d3fa0c1a2f3c7d767f7837
Javokhirbek Tukhtasinov <javohir.manchester@gmail.com>
2021-03-30 23:54:34 +0900

    some changes added"

--- Javohir/projects/movieSeatBooking.js
+++ Javohir/projects/movieSeatBooking.js
@@ -1,2 +1,41 @@
-const container = document.querySelector('.comtainer');
-const seats = document.
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+// save selected movie index
+// function setMovieData(movieIndex, moviePrice) {
+//     localStorage.setItem('selectedMovieIndex', movieIndex);
+//     localStorage.setItem('selectedMoviePrice', moviePrice);
+// }
+//update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    // const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    
+    // localStorage.setItem('selectedSeats', JSON.stringify(seastsIndex));
+    const selectedSeatsCount = selectedSeats.length;
+    
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+})
+
+
+// saet click enent
+container.addEventListener('click', e => {
+    if (
+        e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+});
+
--- karimov/project1/facebook.php
+++ karimov/project1/facebook.php
@@ -11,14 +11,17 @@ if ($_SESSION['is_user_logged_in'] !=1){
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Azamatjon Karimov</title>
 </head>
+
 <body>
-<a href="Logout.php">Logout</a>
+    <a href="Logout.php">Logout</a>
 </body>
+
 </html>
