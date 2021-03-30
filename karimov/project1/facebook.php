a20cb3a3648b1163588a800c03d36b1b44ba4a53
Azamatjon11 <a_karimov2001@mail.ru>
2021-03-30 23:00:29 +0900

    'saving'

--- karimov/project1/facebook.php
+++ karimov/project1/facebook.php
@@ -17,8 +17,12 @@ if ($_SESSION['is_user_logged_in'] !=1){
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Azamatjon Karimov</title>
+    <link rel="stylesheet" href="facebook.css">
 </head>
 <body>
 <a href="Logout.php">Logout</a>
+<hr>
+<h1>hey</h1>
+
 </body>
 </html>
